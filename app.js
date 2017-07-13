e4081edd0685f74227a394e474169bd353e5bc13
nsbradford <nsbradford@gmail.com>
2017-07-12 21:26:47 -0400

    Create different particle.js configs for mobile and desktop.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-console.log('callback - particles.js searching for config...');
-particlesJS.load('particles-js', 'particlesconfig.json', function() {
+// choose a config file based on the window size
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+var json_path = width > 768 ? 'particlesconfig.json' : 'particlesconfig_mobile.json';
+console.log('callback - particles.js using config ' + json_path);
+particlesJS.load('particles-js', json_path, function() {
   console.log('callback - particles.js config loaded');
 });
